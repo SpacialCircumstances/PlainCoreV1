a622570142b1b6381e868a2a3703771866adb578
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-14 15:45:19 +0100

    Simple window in testapp

--- PlainCoreTestapp/Game.cs
+++ PlainCoreTestapp/Game.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PlainCore.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Veldrid;
 
 namespace PlainCoreTestapp
 {
@@ -8,7 +10,14 @@ namespace PlainCoreTestapp
     {
         public void Run()
         {
+            var window = new RenderWindow();
 
+            while(window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(RgbaFloat.CornflowerBlue);
+                window.Display();
+            }
         }
     }
 }
