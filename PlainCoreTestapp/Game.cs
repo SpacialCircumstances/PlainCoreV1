4a83b4bc876af53caf1b1fdca227392ab331f4e7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-14 16:12:50 +0100

    Spritebatch test

--- PlainCoreTestapp/Game.cs
+++ PlainCoreTestapp/Game.cs
@@ -1,6 +1,7 @@
 ﻿using PlainCore.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 using Veldrid;
 
@@ -12,10 +13,26 @@ namespace PlainCoreTestapp
         {
             var window = new RenderWindow();
 
+            var texture1 = PlainCore.Graphics.Texture.FromFile(window.Device, "Images/Planet.png");
+
+            var batch = new SpriteBatch(window.Device);
+
+            DeviceBuffer db = window.Factory.CreateBuffer(new BufferDescription(64, BufferUsage.UniformBuffer));
+
+            Matrix4x4 m = Matrix4x4.Identity;
+            window.Device.UpdateBuffer(db, 0, m);
+            batch.Init();
+            batch.SetWorldMatrix(db);
+
             while(window.IsOpen)
             {
                 window.DispatchEvents();
                 window.Clear(RgbaFloat.CornflowerBlue);
+
+                batch.Begin();
+                batch.Render(texture1, 0.1f, 0.1f, 0.5f, 0.5f);
+                batch.End();
+
                 window.Display();
             }
 
