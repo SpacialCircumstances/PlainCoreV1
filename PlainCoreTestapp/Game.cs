e49af8365554aed69cba68d0a484d217b115b4a5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-16 17:55:19 +0100

    Update Testapp

--- PlainCoreTestapp/Game.cs
+++ PlainCoreTestapp/Game.cs
@@ -1,4 +1,5 @@
 ﻿using PlainCore.Graphics;
+using PlainCore.System;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
@@ -14,21 +15,19 @@ namespace PlainCoreTestapp
             var window = new RenderWindow();
 
             var texture1 = PlainCore.Graphics.Texture.FromFile(window.Device, "Images/Planet.png");
-
+            var texture2 = PlainCore.Graphics.Texture.FromFile(window.Device, "Images/Moon.png");
             var batch = new SpriteBatch(window.Device);
 
-            DeviceBuffer db = window.Factory.CreateBuffer(new BufferDescription(64, BufferUsage.UniformBuffer));
-
-            Matrix4x4 m = Matrix4x4.Identity;
-            window.Device.UpdateBuffer(db, 0, m);
+            var clock = new Clock();
 
             while(window.IsOpen)
             {
                 window.DispatchEvents();
                 window.Clear(RgbaFloat.CornflowerBlue);
-
+                window.GetView().Rotation += 1f * (float)clock.Restart().TotalSeconds;
                 batch.Begin(window);
                 batch.Draw(texture1, 0.1f, 0.1f, 0.5f, 0.5f);
+                batch.Draw(texture2, -0.6f, -0.4f, 0.4f, 0.4f);
                 batch.End();
 
                 window.Display();
