c37117adf4dcb5ace173184500e92e806685b19d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-18 22:09:19 +0100

    VertexArray stub

---
+++ PlainCore/GeometryType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore
+{
+    public enum GeometryType
+    {
+        Points,
+        Lines,
+        LineStrip,
+        Triangles,
+        TriangleStrip
+    }
+}
---
+++ PlainCore/Graphics/VertexArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Veldrid;
+
+namespace PlainCore.Graphics
+{
+    public class VertexArray<T> where T: struct
+    {
+        public VertexArray(GraphicsDevice device, GeometryType geometryType = GeometryType.Points, int initalCapacity = 24)
+        {
+            this.device = device;
+            factory = device.ResourceFactory;
+            GeometryType = geometryType;
+            CreateResources(initalCapacity);
+        }
+
+        private GraphicsDevice device;
+        private ResourceFactory factory;
+
+        private Pipeline pipeline;
+        private CommandList commands;
+        private DeviceBuffer vertexBuffer;
+        private DeviceBuffer worldMatrixBuffer;
+
+        private List<T> vertices;
+
+        #region Public properties
+
+        public GeometryType GeometryType;
+        public int Count { get => vertices.Count; }
+
+        public T this[int index]
+        {
+            get => vertices[index];
+            set => vertices[index] = value;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Add(T vertex)
+        {
+            vertices.Add(vertex);
+        }
+
+        public void Clear()
+        {
+            vertices.Clear();
+        }
+
+        public void Draw(IRenderTarget target)
+        {
+
+        }
+
+        #endregion
+
+        private void CreateResources(int capacity)
+        {
+            
+        }
+    }
+}
