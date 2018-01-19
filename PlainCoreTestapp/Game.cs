14a745082f86a4efbce0fbdfa0ac94de3834e819
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-19 15:06:22 +0100

    Usage example

--- PlainCoreTestapp/Game.cs
+++ PlainCoreTestapp/Game.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 using Veldrid;
+using PlainCore.Graphics.Primitives;
 
 namespace PlainCoreTestapp
 {
@@ -18,6 +19,12 @@ namespace PlainCoreTestapp
             var texture2 = PlainCore.Graphics.Texture.FromFile(window.Device, "Images/Moon.png");
             var batch = new SpriteBatch(window.Device);
 
+            var varray = new VertexArray(window.Device, 4);
+            varray.Add(new VertexPositionColorTexture(new Vector2(-0.5f, -0.5f), RgbaFloat.Orange, new Vector2()));
+            varray.Add(new VertexPositionColorTexture(new Vector2(0.5f, -0.5f), RgbaFloat.Orange, new Vector2()));
+            varray.Add(new VertexPositionColorTexture(new Vector2(-0.5f, 0.5f), RgbaFloat.Orange, new Vector2()));
+            varray.Add(new VertexPositionColorTexture(new Vector2(0.5f, 0.5f), RgbaFloat.Orange, new Vector2()));
+
             var clock = new Clock();
 
             float rot = 0f;
@@ -27,10 +34,7 @@ namespace PlainCoreTestapp
                 window.DispatchEvents();
                 window.Clear(RgbaFloat.CornflowerBlue);
                 rot += (float)clock.Restart().TotalSeconds * 2f;
-                batch.Begin(window);
-                batch.Draw(texture1, 0.1f, 0.1f, 0.5f, 0.5f, 0.5f, 0.5f, rot);
-                batch.End();
-
+                varray.Draw(window, varray.EmptyTexture);
                 window.Display();
             }
 
