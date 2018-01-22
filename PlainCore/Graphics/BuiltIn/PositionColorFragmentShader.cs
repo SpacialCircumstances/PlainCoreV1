03ebc00c97c7c437089547c579873edd3748c2b8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-22 18:42:52 +0100

    Add PositionColor shaders

---
+++ PlainCore/Graphics/BuiltIn/PositionColorFragmentShader.cs
@@ -0,0 +1,31 @@
+﻿using PlainCore.Graphics.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Veldrid;
+
+namespace PlainCore.Graphics.BuiltIn
+{
+    public class PositionColorFragmentShader : Shader
+    {
+        public PositionColorFragmentShader()
+        {
+            Stage = ShaderStages.Fragment;
+            EntryPoint = "FS";
+            GlslShader = new ShaderMemoryResource(Encoding.ASCII.GetBytes(glslShader));
+        }
+
+        private const string glslShader =
+            @"#version 330 core
+
+in vec4 fragColor;
+out vec4 fsout_Color;
+
+void main()
+{
+    fsout_Color = fragColor;
+}
+
+";
+    }
+}
