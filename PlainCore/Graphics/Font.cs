16b8c4f04b768aa3bdfb03c38a6e7e5257b9fbe3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-27 17:44:56 +0100

    Move glyphs to new Text namespace

--- PlainCore/Graphics/Font.cs
+++ PlainCore/Graphics/Font.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Numerics;
-using SixLabors.Fonts;
+using PlainCore.Graphics.Text;
 
 namespace PlainCore.Graphics
 {
--- PlainCore/Graphics/Text/Glyph.cs
+++ PlainCore/Graphics/Text/Glyph.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 
-namespace PlainCore.Graphics
+namespace PlainCore.Graphics.Text
 {
     public struct Glyph
     {
