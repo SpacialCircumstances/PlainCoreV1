4213b69b8c20c90ce312f2bf6e3d69908862d176
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-27 17:42:33 +0100

    Remove font creation using SixLabors.Font, add NRasterizer

--- PlainCore/Graphics/Resources/TruetypeFontResource.cs
+++ PlainCore/Graphics/Resources/TruetypeFontResource.cs
@@ -25,60 +25,7 @@ namespace PlainCore.Graphics.Resources
 
         public Font CreateFont()
         {
-            var collection = new FontCollection();
-            collection.Install(Filename);
-            var ttFont = collection.CreateFont(FontFamily, Size);
-
-            var glyphs = new List<GlyphLayout>();
-
-            var renderOptions = new RendererOptions(ttFont);
-            renderOptions.ApplyKerning = false;
-
-            float offsetX = 0;
-            int yCount = 0;
-
-            for (int i = 0; i < 128; i++)
-            {
-                var character = (char)i;
-                var representation = Convert.ToString(character);
-                var charSize = TextMeasurer.MeasureBounds(representation, renderOptions);
-                var width = charSize.Right;
-                var glyph = new Glyph(character, new Vector2(width, charSize.Bottom));
-
-                if(offsetX + width > MAX_SIZE_X)
-                {
-                    offsetX = 0;
-                    yCount++;
-                }
-
-                offsetX += width;
-
-                var gLayout = new GlyphLayout()
-                {
-                    Glyph = glyph,
-                    BitmapPosition = new Vector2(offsetX, yCount * (Size * 1.5f)),
-                };
-
-                glyphs.Add(gLayout);
-            }
-
-            var lastGlyph = glyphs[glyphs.Count - 1];
-            var bitmapHeight = 400;
-
-            var bitmap = new Image<Rgba32>(MAX_SIZE_X, bitmapHeight);
-            bitmap.Mutate(i => {
-                foreach(var glyph in glyphs)
-                {
-                    i.DrawText(Convert.ToString(glyph.Glyph.Character), ttFont, Rgba32.White, new SixLabors.Primitives.PointF(glyph.BitmapPosition.X, glyph.BitmapPosition.Y));
-                }
-            });
-
-
-
-            var ts = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
-            bitmap.Save(ts + ".png");
-
-            var font = new Font(null, glyphs);
+            var font = new Font(null, null);
             
             return font;
         }
---
+++ PlainCore/Graphics/Text/ImageSharpRasterizer.cs
@@ -0,0 +1,52 @@
+﻿using NRasterizer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Text
+{
+    class ImageSharpRasterizer : IGlyphRasterizer
+    {
+        public int Resolution => throw new NotImplementedException();
+
+        public void BeginRead(int countourCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseFigure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Curve3(double p2x, double p2y, double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Curve4(double p2x, double p2y, double p3x, double p3y, double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndRead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LineTo(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTo(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
