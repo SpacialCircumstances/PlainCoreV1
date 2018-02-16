fed2fc365f3de7d28164d0c770ab0987efe0cbe3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-02-16 17:07:10 +0100

    Add tileset for easier texture region loading

---
+++ PlainCore/Graphics/Tileset.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Veldrid;
+
+namespace PlainCore.Graphics
+{
+    public class Tileset
+    {
+        protected Tileset()
+        {
+
+        }
+
+        private Texture texture;
+        private IReadOnlyDictionary<string, TextureRegion> tiles;
+
+        public static Tileset LoadFromFile(GraphicsDevice device, string regionsFile, string textureFile)
+        {
+            var texture = Texture.FromFile(device, textureFile);
+            var regionsContent = File.ReadAllText(regionsFile);
+            var regions = JsonConvert.DeserializeObject<Dictionary<string, TextureRegion>>(regionsContent);
+
+            return Create(regions, texture);
+        }
+
+        public static Tileset Create(Dictionary<string, TextureRegion> regions, Texture texture)
+        {
+            return new Tileset
+            {
+                texture = texture,
+                tiles = regions
+            };
+        }
+
+        public static void Save(Dictionary<string, TextureRegion> regions, Image<Rgba32> image, string regionsFile, string bitmapFile)
+        {
+            image.Save(bitmapFile);
+            var json = JsonConvert.SerializeObject(regions);
+            File.WriteAllText(regionsFile, json);
+        }
+
+        public Texture TilesetTexture => texture;
+        public IReadOnlyDictionary<string, TextureRegion> Tiles => tiles;
+    }
+}
