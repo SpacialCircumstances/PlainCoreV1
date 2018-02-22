595860c13097c1d88960e6b0742288c5f5a0f3dc
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-02-22 16:40:22 +0100

    Add Position shader

---
+++ PlainCore.Shaders/Position.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ShaderGen;
+using System.Numerics;
+using static ShaderGen.ShaderBuiltins;
+
+[assembly: ShaderSet("Position", "PlainCore.Shaders.Position.VS", "PlainCore.Shaders.Position.FS")]
+
+namespace PlainCore.Shaders
+{
+    public class Position
+    {
+        [ResourceSet(0)]
+        public Matrix4x4 World;
+
+        [VertexShader]
+        public FragmentInput VS(VertexInput input)
+        {
+            FragmentInput output;
+            output.SystemPosition = Mul(World, new Vector4(input.Position, -1, 1));
+            return output;
+        }
+
+        [FragmentShader]
+        public Vector4 FS(FragmentInput input)
+        {
+            return new Vector4(1, 1, 1, 1);
+        }
+
+        public struct VertexInput
+        {
+            [PositionSemantic] public Vector2 Position;
+        }
+
+        public struct FragmentInput
+        {
+            [SystemPositionSemantic] public Vector4 SystemPosition;
+        }
+    }
+}
--- PlainCore/Graphics/BuiltIn/PositionVertexShader.cs
+++ PlainCore/Graphics/BuiltIn/PositionVertexShader.cs
@@ -12,23 +12,7 @@ namespace PlainCore.Graphics.BuiltIn
         {
             Stage = ShaderStages.Vertex;
             EntryPoint = "VS";
-            GlslShader = new ShaderMemoryResource(Encoding.ASCII.GetBytes(glslShader));
+            GlslShader = new ShaderFileResource(BuiltinShaderRepository.SHADER_REPO + "Position-vertex.330.glsl");
         }
-
-        private const string glslShader =
-            @"#version 330 core
-in vec2 Position;
-
-        uniform World
-        {
-            mat4 field_World;
-        };
-
-        void main()
-        {
-            gl_Position = field_World * vec4(Position, 0, 1);
-            gl_Position.z = gl_Position.z * 2.0 - gl_Position.w;
-        }
-";
     }
 }
